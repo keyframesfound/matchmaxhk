2fc7ab196e40bc5b7a276d8e5410753aa3d3728c
keyframesfound <80465725+keyframesfound@users.noreply.github.com>
2026-08-04 20:46:46 +0800

    Request Tutor

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -661,7 +661,7 @@ function Landing() {
                               rel="noreferrer"
                               onClick={() => blurActive()}
                             >
-                              Request this tutor
+                              Request tutor
                             </a>
                           </Button>
                         </div>
